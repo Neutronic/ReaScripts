326ebc6738ac1725e536a23b213546336b04741e
Neutronic <d.band@msn.com>
2020-04-29 07:29:28 +0300

    Update neutronic_Quick Adder 2.lua

--- Utilities/Quick Adder 2/neutronic_Quick Adder 2.lua
+++ Utilities/Quick Adder 2/neutronic_Quick Adder 2.lua
@@ -10,7 +10,7 @@ Links:
   Quick Adder 2 forum thread https://forum.cockos.com/showthread.php?t=232928
   Quick Adder 2 video demo http://bit.ly/seeQA2
 Changelog:
-  + script docking support
+  + support script docking with the title bar menu option (Windows only)
   
   New in v2.15:
   + search and run actions
