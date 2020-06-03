9cac68f6cd9114c6b6c60da5d389f4d8af543126
Neutronic <d.band@msn.com>
2020-06-03 20:27:17 +0300

    Update neutronic_Quick Adder 2.lua

--- Utilities/Quick Adder 2/neutronic_Quick Adder 2.lua
+++ Utilities/Quick Adder 2/neutronic_Quick Adder 2.lua
@@ -10,7 +10,7 @@ Links:
   Quick Adder 2 forum thread https://forum.cockos.com/showthread.php?t=232928
   Quick Adder 2 video demo http://bit.ly/seeQA2
 Changelog:
-  # sanitize backslashes in JS descriptions
+  # escape backslashes in JS descriptions
 
   New in v2.20
   Text selection for editing:
