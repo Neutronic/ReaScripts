cb1916c0b749b782e8c82892cbb0012f9ced5ce1
Neutronic <d.band@msn.com>
2021-08-18 03:48:16 +0300

    Create neutronic_Quick Adder 2 -- optimize GUI size for 1080p.lua

---
+++ Utilities/Quick Adder 2/Actions/neutronic_Quick Adder 2 -- optimize GUI size for 1080p.lua
@@ -0,0 +1,38 @@
+--[[
+Description: Quick Adder 2 -- optimize GUI size for 1080p
+About: Optimizes Quick Adder 2 window size for 1080p screen resolution.
+Version: 1.0
+Author: Neutronic
+Donation: https://paypal.me/SIXSTARCOS
+License: GNU GPL v3
+Links:
+  Neutronic's REAPER forum profile https://forum.cockos.com/member.php?u=66313
+  Quick Adder 2 forum thread https://forum.cockos.com/showthread.php?t=232928
+  Quick Adder 2 video demo http://bit.ly/seeQA2
+Changelog:
+  + initial release
+--]]
+
+if not reaper.CF_EnumerateActions then return end
+
+function getQAid()
+  local i = 0
+  
+  while i <= 65535 do
+    local id, name = reaper.CF_EnumerateActions(0, i, "")
+
+    if name == "Script: neutronic_Quick Adder 2.lua" then return id end
+    i = i + 1
+  end
+end
+
+local qa_id = getQAid()
+
+if not qa_id then reaper.MB("Quick Adder 2 not found.", "ReaScript Error", 0) return end
+
+reaper.SetExtState("Quick Adder", "ACT", "|1080p", false)
+
+if not reaper.HasExtState("Quick Adder", "MSG") and
+  reaper.GetExtState("Quick Adder", "MSG") ~= "reopen" then
+  reaper.Main_OnCommand(qa_id, 0)
+end
